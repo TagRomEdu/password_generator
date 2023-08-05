5ba97877622b07d266766dd256616de47d48fcdc
TagRomEdu <qwertystyles@gmail.com>
2023-08-05 12:59:42 +0500

    added main

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-import random
 from func import check_is_num, check_is_correct, delete_symb, generate_password
+
 
 DIGITS = '0123456789'
 LOWERCASE_LETTER = 'abcdefghijklmnopqrstuvwxyz'
 UPPERCASE_LETTER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 PUNCTUATION = '!#$%&*+-=?@^_'
 
-chars = ''
 
-password_count = check_is_num(input("Сколько паролей желаешь создать? Введи число:\n"))
+def main():
 
-len_password = check_is_num(input("Длина одного пароля? Число:\n"))
+    chars = ''
 
-digit_in_pass = check_is_correct(input(f"Будет ли он содержать {DIGITS}? Да или нет:\n"))
-if digit_in_pass.lower() == "да":
-    chars += DIGITS
+    password_count = check_is_num(input("Сколько паролей желаешь создать? Введи число:\n"))
 
-upper_letters = check_is_correct(input(f"Будет ли он содержать прописные {UPPERCASE_LETTER}? Да или нет:\n"))
-if upper_letters.lower() == "да":
-    chars += UPPERCASE_LETTER
+    len_password = check_is_num(input("Длина одного пароля? Число:\n"))
 
-lower_letters = check_is_correct(input(f"Будет ли он содержать строчные {LOWERCASE_LETTER}? Да или нет:\n"))
-if lower_letters.lower() == "да":
-    chars += LOWERCASE_LETTER
+    digit_in_pass = check_is_correct(input(f"Будет ли он содержать {DIGITS}? Да или нет:\n"))
+    if digit_in_pass.lower() == "да":
+        chars += DIGITS
 
-symbols = check_is_correct(input(f"Включим символы {PUNCTUATION}? Да или нет:\n"))
-if symbols.lower() == "да":
-    chars += PUNCTUATION
+    upper_letters = check_is_correct(input(f"Будет ли он содержать прописные {UPPERCASE_LETTER}? Да или нет:\n"))
+    if upper_letters.lower() == "да":
+        chars += UPPERCASE_LETTER
 
-exclude_symbols = check_is_correct(input("Будем исключать неоднозначные символы: il1Lo0O? Да или нет:\n"))
-if exclude_symbols.lower() == "да":
-    chars = delete_symb(chars)
+    lower_letters = check_is_correct(input(f"Будет ли он содержать строчные {LOWERCASE_LETTER}? Да или нет:\n"))
+    if lower_letters.lower() == "да":
+        chars += LOWERCASE_LETTER
+
+    symbols = check_is_correct(input(f"Включим символы {PUNCTUATION}? Да или нет:\n"))
+    if symbols.lower() == "да":
+        chars += PUNCTUATION
+
+    exclude_symbols = check_is_correct(input("Будем исключать неоднозначные символы: il1Lo0O? Да или нет:\n"))
+    if exclude_symbols.lower() == "да":
+        chars = delete_symb(chars)
+
+    generate_password(password_count, chars, len_password)
+
+
+if __name__ == '__main__':
+    main()
