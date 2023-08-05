bd6b1445ee75b824199e2dbca5495fea8cf3c2f0
TagRomEdu <qwertystyles@gmail.com>
2023-08-05 12:44:23 +0500

    added descriptions for functions

--- func.py
+++ func.py
@@ -1,21 +1,26 @@
 def check_is_num(string: str) -> str:
+    """
+    To check the string is digit
+    """
     while not string.isdigit():
         string = input("Введите число:\n")
     return string
 
 
 def check_is_correct(string: str) -> str:
+    """
+    To check the string is YES or NO
+    """
     while string.lower() != "да" and string.lower() != "нет":
         string = input("Введите да или нет...\n")
     return string
 
 
 def delete_symb(string: str) -> str:
+    """
+    To delete specific symbols
+    """
     for letter in 'il1Lo0O':
         if letter in string:
             string = string.replace(letter, '')
     return string
-
-
-def generate_password():
-    pass
