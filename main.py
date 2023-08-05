2c575c25cbc26346e594f4503d847637e4cac98a
TagRomEdu <qwertystyles@gmail.com>
2023-08-05 12:30:11 +0500

    added funcs from func.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from func import check_is_num, check_is_correct, delete_symb, generate_password
 
 DIGITS = '0123456789'
 LOWERCASE_LETTER = 'abcdefghijklmnopqrstuvwxyz'
@@ -7,11 +8,26 @@ PUNCTUATION = '!#$%&*+-=?@^_'
 
 chars = ''
 
-password_count = int(input("Сколько паролей желаешь создать? Введи число:\n"))
-len_password = int(input("Длина одного пароля? Число:\n"))
-digit_in_pass = input(f"Будет ли он содержать {DIGITS}? Да или нет:\n")
-upper_letters = input(f"Будет ли он содержать прописные {UPPERCASE_LETTER}? Да или нет:\n")
-lower_letters = input(f"Будет ли он содержать строчные {LOWERCASE_LETTER}? Да или нет:\n")
-symbols = input(f"Включим символы {PUNCTUATION}? Да или нет:\n")
-exclude_symbols = input("Будем исключать неоднозначные символы: il1Lo0O?\n")
+password_count = check_is_num(input("Сколько паролей желаешь создать? Введи число:\n"))
 
+len_password = check_is_num(input("Длина одного пароля? Число:\n"))
+
+digit_in_pass = check_is_correct(input(f"Будет ли он содержать {DIGITS}? Да или нет:\n"))
+if digit_in_pass.lower() == "да":
+    chars += DIGITS
+
+upper_letters = check_is_correct(input(f"Будет ли он содержать прописные {UPPERCASE_LETTER}? Да или нет:\n"))
+if upper_letters.lower() == "да":
+    chars += UPPERCASE_LETTER
+
+lower_letters = check_is_correct(input(f"Будет ли он содержать строчные {LOWERCASE_LETTER}? Да или нет:\n"))
+if lower_letters.lower() == "да":
+    chars += LOWERCASE_LETTER
+
+symbols = check_is_correct(input(f"Включим символы {PUNCTUATION}? Да или нет:\n"))
+if symbols.lower() == "да":
+    chars += PUNCTUATION
+
+exclude_symbols = check_is_correct(input("Будем исключать неоднозначные символы: il1Lo0O? Да или нет:\n"))
+if exclude_symbols.lower() == "да":
+    chars = delete_symb(chars)
